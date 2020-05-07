43428204cec9984fe8c8fcd9c62f1c609cda163b
Oswell Chan <oswellchan@gmail.com>
2020-05-07 10:06:20 +0800

    Implement double tap zoom

--- lib/src/reader/reader.dart
+++ lib/src/reader/reader.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+
 import 'package:cached_network_image/cached_network_image.dart';
 
 import '../database/db.dart';
@@ -94,19 +97,136 @@ class Reader extends StatefulWidget {
 Widget _buildPage(String pageUrl) {
   return Container(
     padding: const EdgeInsets.only(bottom: 10),
-    child: CachedNetworkImage(
-      imageUrl: pageUrl,
-      placeholder: (context, url) => Center(
-        child: SizedBox(
-          child: CupertinoActivityIndicator(),
-          height:70.0,
-          width: 70.0,
+    child: ZoomableWidget(
+      child: CachedNetworkImage(
+        imageUrl: pageUrl,
+        placeholder: (context, url) => Center(
+          child: SizedBox(
+            child: CupertinoActivityIndicator(),
+            height:70.0,
+            width: 70.0,
+          )
+        ),
+        errorWidget: (context, url, error) => Container(
+          color: CupertinoColors.systemGrey,
+          child: Icon(Icons.error),
         )
       ),
-      errorWidget: (context, url, error) => Container(
-        color: CupertinoColors.systemGrey,
-        child: Icon(Icons.error),
-      )
     ),
   );
+}
+
+class ZoomableWidgetState extends State<ZoomableWidget> 
+  with SingleTickerProviderStateMixin {
+  var matrix = Matrix4.identity();
+  var zoomOffset = Offset.zero;
+  var originalSize;
+  var oldGlobalPoint = Offset.zero;
+  var _scale = 1.0;
+
+  AnimationController _zoomController;
+
+  @override
+  void initState() {
+    super.initState();
+    _zoomController = AnimationController(
+        vsync: this,
+        lowerBound: 1 / 1.2,
+        upperBound: 1.2,
+        duration: Duration(seconds: 1));
+    _zoomController.addListener(() {
+      setState(() {
+        _scale = _zoomController.value;
+        if (_scale == 1 / 1.2) {
+          matrix = Matrix4.identity();
+        } else {
+          matrix.scale(_scale, _scale);
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _zoomController.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: GestureDetector(
+        child: Transform(
+          transform: matrix,
+          origin: zoomOffset,
+          child: widget.child
+        ),
+        onTapDown: _onTapDown,
+      ),
+    );
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    if (matrix == Matrix4.identity()) {
+      var x = details.localPosition.dx;
+      var y = details.localPosition.dy;
+      zoomOffset = Offset(x, y);
+    }
+
+    var currGlobalPointX = details.globalPosition.dx;
+    var currGlobalPointY = details.globalPosition.dy;
+    var currGlobalPoint = Offset(currGlobalPointX, currGlobalPointY);
+
+    if (_isDoubleTap(currGlobalPoint)) {
+      _onDoubleTap();
+    }
+
+    oldGlobalPoint = currGlobalPoint;
+    Timer(Duration(milliseconds: 500), () {
+      oldGlobalPoint = Offset.zero;
+    });
+  }
+
+  bool _isDoubleTap(Offset currGlobalPoint) {
+    if ((oldGlobalPoint.dx - currGlobalPoint.dx).abs() < 20 &&
+      (oldGlobalPoint.dy - currGlobalPoint.dy).abs() < 20) {
+      return true;
+    }
+    return false;
+  }
+
+  void _onDoubleTap() {
+    // Prevent interaction
+    if (_zoomController.isAnimating) {
+      return;
+    }
+
+    oldGlobalPoint = Offset.zero;
+    setState(() {
+      if (matrix == Matrix4.identity()) {
+        _zoomController.forward(from: 1.0);
+      } else {
+        _zoomController.reverse(from: 1.0);
+      }
+    });
+  }
+
+  // Offset _clampOffset(Offset offset) {
+  //   final Size size = context.size;
+  //   final Offset minOffset =
+  //       new Offset(size.width, size.height) * (1.0 - _scale);
+  //   return new Offset(
+  //       offset.dx.clamp(minOffset.dx, 0.0), offset.dy.clamp(minOffset.dy, 0.0));
+  // }
+}
+
+class ZoomableWidget extends StatefulWidget {
+  final Widget child;
+
+  ZoomableWidget({
+    @required this.child,
+  });
+
+  @override
+  ZoomableWidgetState createState() => ZoomableWidgetState();
 }
